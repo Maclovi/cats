10282d5926b54d9b60c620d0665097f27853ad2d
Sergey <yavorskyserge@gmail.com>
2024-10-19 00:04:44 +0300

    refactoring: remove Depends, add dishka

--- src/cats/web.py
+++ src/cats/web.py
@@ -1,19 +1,21 @@
 from logging import getLogger
 
+from dishka.integrations.fastapi import setup_dishka
 from fastapi import FastAPI
 
 from cats.api import breeds, cats, index
-from cats.di import init_dependencies
+from cats.ioc import init_async_container
 
 logger = getLogger(__name__)
 
 
 def create_app() -> FastAPI:
+    container = init_async_container()
     app = FastAPI()
     app.include_router(cats.router)
     app.include_router(breeds.router)
     app.include_router(index.router)
-    init_dependencies(app)
-    logger.info("App created")
+    setup_dishka(container, app)
 
+    logger.info("App created")
     return app
