cb9a4682d4251d5fae381db3f32748e68e532ad0
Sergey <yavorskyserge@gmail.com>
2024-10-19 11:57:15 +0300

    add lifespan

--- src/cats/web.py
+++ src/cats/web.py
@@ -1,3 +1,5 @@
+from collections.abc import AsyncIterator
+from contextlib import asynccontextmanager
 from logging import getLogger
 
 from dishka.integrations.fastapi import setup_dishka
@@ -9,9 +11,15 @@ from cats.ioc import init_async_container
 logger = getLogger(__name__)
 
 
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncIterator[None]:
+    yield None
+    await app.state.dishka_container.close()
+
+
 def create_app() -> FastAPI:
     container = init_async_container()
-    app = FastAPI()
+    app = FastAPI(lifespan=lifespan)
     app.include_router(cats.router)
     app.include_router(breeds.router)
     app.include_router(index.router)
